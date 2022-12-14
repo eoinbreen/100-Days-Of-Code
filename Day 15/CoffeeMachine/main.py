a1685b8c15d0edc1435fb6c54acaf98cbaf05bc1
eoinbreen <e.breen185@gmail.com>
2022-12-14 22:06:19 +0000

    Finished Coffee Machine

--- Day 15/CoffeeMachine/main.py
+++ Day 15/CoffeeMachine/main.py
@@ -5,12 +5,15 @@ from data import MENU
 
 def print_resources():
     """Prints a report of what resources the user has"""
-    for key in resources:
-        print(f"{key} - {resources[key]}")
+    print(f"Water : {resources['water']}ml")
+    print(f"Milk : {resources['milk']}ml")
+    print(f"Coffee : {resources['coffee']}g")
+    print(f"Money : ${format(resources['money'], '.2f')} ")
 
 
 def take_resources(drink):
     resources["money"] -= MENU[drink]["cost"]
+    resources["money"] = round(resources["money"], 2)
     resources["water"] -= MENU[drink]["ingredients"]["water"]
     resources["milk"] -= MENU[drink]["ingredients"]["milk"]
     resources["coffee"] -= MENU[drink]["ingredients"]["coffee"]
@@ -22,15 +25,15 @@ def check_resources(drink):
         resources["money"] = 0
         return False
     elif resources["water"] < MENU[drink]["ingredients"]["water"]:
-        return False
+        print(f"You do not have enough Water to make a {drink}")
     elif resources["milk"] < MENU[drink]["ingredients"]["milk"]:
-        return False
+        print(f"You do not have enough Milk to make a {drink}")
     elif resources["coffee"] < MENU[drink]["ingredients"]["coffee"]:
-        return False
+        print(f"You do not have enough Coffee to make a {drink}")
     else:
-        print(f"Here is your {drink}, enjoy!")
         take_resources(drink)
-        return True
+        print(f"Here is your {drink}, enjoy!")
+        print(f"you have ${format(resources['money'], '.2f')} change left")
 
 
 def calculate_money(num_pennies, num_nickles, num_dimes, num_quarters):
@@ -39,23 +42,37 @@ def calculate_money(num_pennies, num_nickles, num_dimes, num_quarters):
     nickles = coins["nickle"] * num_nickles
     dimes = coins["dime"] * num_dimes
     quarters = coins["quarter"] * num_quarters
-    total_money = pennies + nickles + dimes + quarters
+    total_coins = pennies + nickles + dimes + quarters
+    resources['money'] += total_coins
 
-    return total_money
+
+def add_money():
+    """Takes in coins then adds their combined value to the users money"""
+    print("Please insert some coins")
+    pennies = int(input("How many pennies would you like to insert? "))
+    nickles = int(input("How many nickles would you like to insert? "))
+    dimes = int(input("How many dimes would you like to insert? "))
+    quarters = int(input("How many quarters would you like to insert? "))
+    calculate_money(pennies, nickles, dimes, quarters)
 
 
-available_drinks = []
+valid_choices = ["report", "off", "add"]
 for key in MENU:
-    available_drinks.append(key)
+    valid_choices.append(key)
 
-print(available_drinks)
-# TODO: Ask the user what they would like
+on = True
+while on:
+    user_choice = input("What would you like? (espresso, latte, cappuccino) Type report for a list of resources. : ").lower()
+    while user_choice not in valid_choices:
+        user_choice = input("That is not a valid choice, please choose one of the above drinks or report : ").lower()
+    if user_choice == "off":
+        on = False
+    elif user_choice == "report":
+        print_resources()
+    else:
+        add_money()
+        check_resources(user_choice)
 
-# TODO: Ask the user to input money into the machine
 
-# TODO: Turn off the machine
+print("turning off machine")
 
-
-resources["money"] = calculate_money(12, 12, 12, 12)
-check_resources("latte")
-print_resources()
