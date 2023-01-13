08b0b9a26c76a40f0d18bd4f9b38d39886a25622
eoinbreen <e.breen185@gmail.com>
2023-01-13 17:51:16 +0000

    Created cars and made them move

--- Day 23 - Turtle Crossing Project/Turtle_Crossing/car_manager.py
+++ Day 23 - Turtle Crossing Project/Turtle_Crossing/car_manager.py
@@ -1,7 +1,31 @@
+from turtle import Turtle   # https://docs.python.org/3/library/turtle.html
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+POSITION = (305, 0)
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(POSITION)
+        self.cars = []
+        self.move_distance = STARTING_MOVE_DISTANCE
+
+    def move_cars(self):
+        for car in self.cars:
+            car.forward(self.move_distance)
+
+    def create_car(self):
+        new_car = Turtle("square")
+        new_car.penup()
+        new_car.setheading(180)
+        new_car.shapesize(stretch_len=2, stretch_wid=1)
+        rand_y = random.randint(-250, 250)
+        new_car.goto(305, rand_y)
+        new_car.color(random.choice(COLORS))
+        self.cars.append(new_car)
--- Day 23 - Turtle Crossing Project/Turtle_Crossing/main.py
+++ Day 23 - Turtle Crossing Project/Turtle_Crossing/main.py
@@ -4,6 +4,8 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+CAR_TIMER = 6  # Cooldown on creating cars, decreases with each update
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("Turtle Crossing")
@@ -11,15 +13,22 @@ screen.tracer(0)
 
 player = Player()
 scoreboard = Scoreboard()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkeypress(player.up, "Up")
 
+
 game_is_on = True
-car_timer = 6
+car_timer = CAR_TIMER
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_timer -= 1
+    if car_timer == 0:
+        car_manager.create_car()
+        car_timer = CAR_TIMER
+    car_manager.move_cars()
     if player.ycor() > 260:
         scoreboard.increase_level()
         player.reset_position()
