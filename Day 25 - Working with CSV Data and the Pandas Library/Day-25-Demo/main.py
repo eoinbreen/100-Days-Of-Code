e584378660a7e5135044a893083f28cfa8ce9a78
eoinbreen <e.breen185@gmail.com>
2023-01-17 19:47:53 +0000

    Started using Pandas Library

--- Day 25 - Working with CSV Data and the Pandas Library/Day-25-Demo/main.py
+++ Day 25 - Working with CSV Data and the Pandas Library/Day-25-Demo/main.py
@@ -1,10 +1,17 @@
-import csv
+# CSV CODE - A LOT OF WORK FOR ONE ARRAY
+# import csv
+#
+# with open("weather_data.csv", mode="r") as file:
+#     weather_data = csv.reader(file)
+#     temperatures = []
+#     for row in weather_data:
+#         if row[1] != "temp":
+#             rows_temperature = int(row[1])
+#             temperatures.append(rows_temperature)
+#     print(temperatures)
 
-with open("weather_data.csv", mode="r") as file:
-    weather_data = csv.reader(file)
-    temperatures = []
-    for row in weather_data:
-        if row[1] != "temp":
-            rows_temperature = int(row[1])
-            temperatures.append(rows_temperature)
-    print(temperatures)
+import pandas  # https://pandas.pydata.org/docs/
+
+weather_data = pandas.read_csv("weather_data.csv")
+
+print(weather_data["temp"])
