4bb31b3227a2a0d7858f3259aef92d87a1db7ee4
eoinbreen <e.breen185@gmail.com>
2023-01-24 20:15:50 +0000

    Learned to use buttons and input boxes

--- Day 27 -Tkinter, args, kwargs and Creating GUI Programs/Day-27-Demo/main.py
+++ Day 27 -Tkinter, args, kwargs and Creating GUI Programs/Day-27-Demo/main.py
@@ -1,12 +1,26 @@
-import tkinter  # https://docs.python.org/3/library/tkinter.html#
+from tkinter import *  # https://docs.python.org/3/library/tkinter.html#    http://tcl.tk/man/tcl8.6/TkCmd/entry.htm
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")  # Creates Label in the middle of the screen
+my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
+my_label.pack()  # Creates Label in the middle of the screen
+
+
+# Button
+def button_clicked():
+    my_label["text"] = my_input.get()
+
+
+button = Button(text="Click Me", command=button_clicked)
+button.pack()
+
+# Entry
+my_input = Entry(width=10)
+my_input.pack()
+
 
 window.mainloop()  # Keeps window open - must be last line in code
 
