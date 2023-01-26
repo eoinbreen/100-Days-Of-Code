16b9b9e6fa78be57122b6a87cda920013bc22a5c
eoinbreen <e.breen185@gmail.com>
2023-01-26 20:07:08 +0000

    Added timer to Pomodoro Application and learned Dynamic Typing

--- Day 28 - Tkinter, Dynamic Typing and the Pomodoro Application/pomodoro-start/main.py
+++ Day 28 - Tkinter, Dynamic Typing and the Pomodoro Application/pomodoro-start/main.py
@@ -11,16 +11,34 @@ SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 CHECKMARK = "✔"
 
+
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+def start_timer():
+    count_down(5 * 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def count_down(count):
+    count_min = int(count/60)
+    count_secs = count % 60
+
+    if count_secs < 10:
+        count_secs = f"0{count_secs}"  # Add a 0 in front of single digit number to make it look like a timer
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_secs}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+
 
 title_label = Label(text="Timer", font=FONT, bg=YELLOW, fg=GREEN)
 title_label.grid(column=1, row=0)
@@ -28,12 +46,12 @@ title_label.grid(column=1, row=0)
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100, 130, text="00:00", fill="white", font=FONT)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=FONT)
 canvas.grid(column=1, row=1)
 
 
 def start():
-    checkmark_label["text"] += CHECKMARK
+    start_timer()
 
 
 start_button = Button(text="Start", command=start)
@@ -44,8 +62,9 @@ def reset():
     checkmark_label["text"] = checkmark_label["text"][:-1]
 
 
-start_button = Button(text="Reset", command=reset)
-start_button.grid(column=2, row=2)
+reset_button = Button(text="Reset", command=reset)
+reset_button.grid(column=2, row=2)
+
 
 checkmark_label = Label(text=CHECKMARK, bg=YELLOW, fg=GREEN)
 checkmark_label.grid(column=1, row=3)
