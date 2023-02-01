2349baf0c647776a0b4a734f0da748212ea50a76
eoinbreen <e.breen185@gmail.com>
2023-02-01 22:08:23 +0000

    Learned to raise my own exceptions

--- Day 30 - Errors, Exceptions and JSON Data/Day-30-Demo/main.py
+++ Day 30 - Errors, Exceptions and JSON Data/Day-30-Demo/main.py
@@ -1,24 +1,29 @@
 # File not Found
-try:
-    file = open("file.txt")
-    dictionary = {"Key": "Value"}
-    value = dictionary["Key"]
-except FileNotFoundError:
-    file = open("file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist in the dictionary")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File now closed")
+# try:
+#     file = open("file.txt")
+#     dictionary = {"Key": "Value"}
+#     value = dictionary["Key"]
+# except FileNotFoundError:
+#     file = open("file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist in the dictionary")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File now closed")
+#     raise TypeError("This is an error that I made up")
 
+height = float(input("Height (Meters): "))
+weight = int(input("Weight: "))
 
-# Key Error
-# dictionary = {"Key": "Value"}
-# value = dictionary["Another Key"]
+if height > 3:
+    raise ValueError("Human Height should not be over 3 Meters")
+bmi = weight / height ** 2
+print(bmi)
+
 
 # Index Error
 # fruits = ["Apple", "Orange", "Grape"]
