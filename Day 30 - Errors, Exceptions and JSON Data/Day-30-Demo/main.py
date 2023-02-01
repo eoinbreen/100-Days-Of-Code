1d5dca5378b5825b45844d21e76138c9fe20794f
eoinbreen <e.breen185@gmail.com>
2023-02-01 21:46:18 +0000

    Learned most common errors and what causes them

---
+++ Day 30 - Errors, Exceptions and JSON Data/Day-30-Demo/main.py
@@ -0,0 +1,15 @@
+# File not Found
+with open("file.txt") as file:
+    file.read()
+
+# Key Error
+# dictionary = {"Key": "Value"}
+# value = dictionary["Another Key"]
+
+# Index Error
+# fruits = ["Apple", "Orange", "Grape"]
+# fruit = fruits[3]
+
+# Type Error
+# text = "abc"
+# print(text + 5)
