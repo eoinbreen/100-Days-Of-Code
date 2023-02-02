eeb0e579df3d1a5f16671260971ec84b343a4c9a
eoinbreen <e.breen185@gmail.com>
2023-02-02 21:38:05 +0000

    Updated NATO project with error handling

--- Day 26 - List Comprehension and the NATO alphabet/NATO-alphabet-start/NATO-alphabet-start/main.py
+++ Day 26 - List Comprehension and the NATO alphabet/NATO-alphabet-start/NATO-alphabet-start/main.py
@@ -8,7 +8,17 @@ import pandas
 alphabet_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_codes = {row.letter: row.code for (index, row) in alphabet_df.iterrows()}
 
+
 # TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_name = input("What is your name? ").upper
-nato_name = [nato_codes[letter] for letter in user_name]
-print(nato_name)
+def generate_nato():
+    user_name = input("What is your name? ").upper()
+    try:
+        nato_name = [nato_codes[letter] for letter in user_name]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_nato()
+    else:
+        print(nato_name)
+
+
+generate_nato()
