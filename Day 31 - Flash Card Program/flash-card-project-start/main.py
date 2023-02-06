1a2153d075faf644fdc9e23364c4ad9387e92320
eoinbreen <e.breen185@gmail.com>
2023-02-06 17:17:19 +0000

    Read CSV and got a random word to display to the screen each time buttons were pressed

--- Day 31 - Flash Card Program/flash-card-project-start/main.py
+++ Day 31 - Flash Card Program/flash-card-project-start/main.py
@@ -1,6 +1,17 @@
 from tkinter import *  # https://docs.python.org/3/library/tkinter.html#    http://tcl.tk/man/tcl8.6/TkCmd/entry.htm
-
+import pandas
+import random
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+words_to_learn = data.to_dict(orient="records")
+
+
+def next_card():
+    word = random.choice(words_to_learn)["French"]
+    canvas.itemconfig(language_text, text="French")
+    canvas.itemconfig(word_text, text=f"{word}")
+
 
 window = Tk()
 window.title("Flashy")
@@ -15,11 +26,11 @@ word_text = canvas.create_text(400, 263, text="Word", font=("Ariel", 60, "bold")
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_image = PhotoImage(file="images/wrong.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=next_card)
 wrong_button.grid(column=0, row=1)
 
 right_image = PhotoImage(file="images/right.png")
-right_button = Button(image=right_image, highlightthickness=0)
+right_button = Button(image=right_image, highlightthickness=0, command=next_card)
 right_button.grid(column=1, row=1)
 
 window.mainloop()
