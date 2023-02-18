a384db4b4f9536150687530f083aca00e902e83b
eoinbreen <e.breen185@gmail.com>
2023-02-18 18:46:19 +0000

    Learned to send an email on a specific date using datetime

--- Day 32 - Send Email (smtplib) & Manage Dates (datetime)/Birthday Wisher (Day 32) start/main.py
+++ Day 32 - Send Email (smtplib) & Manage Dates (datetime)/Birthday Wisher (Day 32) start/main.py
@@ -1,23 +1,31 @@
 import smtplib
-
-# my_email = "eoinbreen185@gmail.com"
-# password = "vtdpykloradcdwxv"
-# recipient = "eoinbreen185@yahoo.com"
-#
-# with smtplib.SMTP("smtp.gmail.com") as connection:
-#     connection.starttls()
-#     connection.login(user=my_email, password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs=recipient,
-#         msg="Subject:Hello\n\nThis is the body of my email"
-#     )
-
 import datetime as dt
+import random
+
+
+my_email = "eoinbreen185@gmail.com"
+password = "vtdpykloradcdwxv"
+recipient = "eoinbreen185@yahoo.com"
+
 now = dt.datetime.now()
 day = now.weekday()
-print(day)
+if day == 5:
+    with open("quotes.txt", "r") as file:
+        quotes = file.readlines()
+    quote = random.choice(quotes)
 
-date_of_birth = dt.datetime(year=1992, month=5, day=18, hour=4)
-print(date_of_birth)
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=recipient,
+            msg=f"Subject:Monday Motivation\n\n{quote}"
+        )
+
+
+
+
+
+
 
