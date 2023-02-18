ed656718909e57b9fa2e4a6dcf1f890c7906defb
eoinbreen <e.breen185@gmail.com>
2023-02-18 17:31:04 +0000

    Learned to use the datetime module

--- Day 32 - Send Email (smtplib) & Manage Dates (datetime)/Birthday Wisher (Day 32) start/main.py
+++ Day 32 - Send Email (smtplib) & Manage Dates (datetime)/Birthday Wisher (Day 32) start/main.py
@@ -1,14 +1,23 @@
 import smtplib
 
-my_email = "eoinbreen185@gmail.com"
-password = "vtdpykloradcdwxv"
-recipient = "eoinbreen185@yahoo.com"
+# my_email = "eoinbreen185@gmail.com"
+# password = "vtdpykloradcdwxv"
+# recipient = "eoinbreen185@yahoo.com"
+#
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs=recipient,
+#         msg="Subject:Hello\n\nThis is the body of my email"
+#     )
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs=recipient,
-        msg="Subject:Hello\n\nThis is the body of my email"
-    )
+import datetime as dt
+now = dt.datetime.now()
+day = now.weekday()
+print(day)
+
+date_of_birth = dt.datetime(year=1992, month=5, day=18, hour=4)
+print(date_of_birth)
+
