3243ccb8ec88abd061fceaa227f99a1a7e42ac90
eoinbreen <e.breen185@gmail.com>
2023-02-24 21:32:25 +0000

    Added the Question to the User Interface

--- Day 35 - API Practice - Creating a GUI  Quiz App/quizzler-app-start/ui.py
+++ Day 35 - API Practice - Creating a GUI  Quiz App/quizzler-app-start/ui.py
@@ -1,10 +1,14 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 FONT = ("Ariel", 20, "italic")
 
+
 class QuizInterface:
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20, pady=20, bg=THEME_COLOR)
@@ -13,7 +17,7 @@ class QuizInterface:
         self.score_label.grid(column=1, row=0)
 
         self.canvas = Canvas(width=300, height=250,  highlightthickness=0)
-        self.question_text = self.canvas.create_text(150, 125, text="Question", font=FONT, fill= THEME_COLOR)
+        self.question_text = self.canvas.create_text(150, 125, text="Question", width=280, font=FONT, fill=THEME_COLOR)
         self.canvas.grid(column=0, row=1, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="images/true.png")
@@ -24,4 +28,9 @@ class QuizInterface:
         self.false_button = Button(image=false_image, highlightthickness=0)
         self.false_button.grid(column=1, row=2)
 
+        self.get_next_question()
         self.window.mainloop()
+
+    def get_next_question(self):
+        q_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=q_text)
