415a75f9c259ad9ae8b943d23ca426c7462a0aec
eoinbreen <e.breen185@gmail.com>
2023-03-08 21:42:11 +0000

    Learned to add Data to the Grid

--- Day 37 - Habit Tracking Project - API Post Requests & Headers/Habit-Tracker/main.py
+++ Day 37 - Habit Tracking Project - API Post Requests & Headers/Habit-Tracker/main.py
@@ -2,6 +2,7 @@ import requests
 
 USERNAME = "eoinbreen"
 TOKEN = "12345678"
+GRAPH_ID = "steps"
 
 pixela_endpoint = "https://pixe.la/v1/users"
 
@@ -12,12 +13,12 @@ user_params = {
     "notMinor": "yes",
 }
 
-# response = requests.post(url=pixela_endpoint, json=user_params)
-# print(response.text)
+# response = requests.post(url=pixela_endpoint, json=user_params) # Creating Username
+
 
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 graph_params = {
-    "id": "steps",
+    "id": GRAPH_ID,
     "name": "Step Counter",
     "unit": "Steps",
     "type": "int",
@@ -26,9 +27,18 @@ graph_params = {
 headers = {
     "X-USER-TOKEN": TOKEN,
 }
-requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+# requests.post(url=graph_endpoint, json=graph_params, headers=headers)  # Creating Grid
 
-# https://pixe.la/v1/users/eoinbreen/graphs/steps.html
+html_endpoint = f"https://pixe.la/v1/users/{USERNAME}/graphs/{GRAPH_ID}"
+
+html_params = {
+    "date": "20230306",
+    "quantity": "12655"
+}
+
+response = requests.post(url=html_endpoint, json=html_params, headers=headers)
+print(response.text)
+# https://pixe.la/v1/users/eoinbreen/graphs/steps
 
 
 
