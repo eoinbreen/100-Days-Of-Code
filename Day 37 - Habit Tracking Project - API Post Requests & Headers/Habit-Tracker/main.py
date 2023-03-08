88f1e2b584e7012d0f2b1e21ff9bad5fc3793f5f
eoinbreen <e.breen185@gmail.com>
2023-03-08 21:25:03 +0000

    Created Step Counter Graph

--- Day 37 - Habit Tracking Project - API Post Requests & Headers/Habit-Tracker/main.py
+++ Day 37 - Habit Tracking Project - API Post Requests & Headers/Habit-Tracker/main.py
@@ -1,14 +1,37 @@
 import requests
 
+USERNAME = "eoinbreen"
+TOKEN = "12345678"
+
 pixela_endpoint = "https://pixe.la/v1/users"
 
 user_params = {
-    "token": "12345678",
-    "username": "eoinbreen",
+    "token": TOKEN,
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(url=pixela_endpoint, json=user_params)
+# response = requests.post(url=pixela_endpoint, json=user_params)
+# print(response.text)
 
-print(response.text)
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_params = {
+    "id": "steps",
+    "name": "Step Counter",
+    "unit": "Steps",
+    "type": "int",
+    "color": "momiji"
+}
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+requests.post(url=graph_endpoint, json=graph_params, headers=headers)
+
+# https://pixe.la/v1/users/eoinbreen/graphs/steps.html
+
+
+
+
+
+
