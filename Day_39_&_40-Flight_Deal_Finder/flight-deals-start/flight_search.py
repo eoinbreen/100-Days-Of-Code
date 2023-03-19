d5478c7b7f03fad7bb5e30a6e98b2cbec9d8c305
eoinbreen <e.breen185@gmail.com>
2023-03-19 20:35:12 +0000

    Got Lowest price for each of the cities in the list within next 6 months

--- Day_39_&_40-Flight_Deal_Finder/flight-deals-start/flight_search.py
+++ Day_39_&_40-Flight_Deal_Finder/flight-deals-start/flight_search.py
@@ -1,10 +1,12 @@
 import requests
 import os
+from flight_data import FlightData
 TEQUELA_ENDPOINT = "https://api.tequila.kiwi.com"
 TEQUELA_KEY = os.environ.get("TEQUELA_KEY")
-DUBLIN = "DUB"
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
+    # This class is responsible for talking to the Flight Search API.
 
     def get_iata_code(self, city):
         query_endpoint = f"{TEQUELA_ENDPOINT}/locations/query"  #  https://tequila.kiwi.com/portal/docs/tequila_api/locations_api
@@ -21,24 +23,40 @@ class FlightSearch:
         iata_code = results[0]["code"]
         return iata_code
 
+    def get_lowest_price(self, origin_city_code, destination_city_code, from_time, to_time):
+        search_endpoint = f"{TEQUELA_ENDPOINT}/v2/search"  # https://tequila.kiwi.com/portal/docs/tequila_api/search_api
 
-    def get_prices(self):
-        search_endpoint = f"{TEQUELA_ENDPOINT}/v2/search"
         headers = {
             "apikey": TEQUELA_KEY
         }
         params = {
-            "fly_from": DUBLIN,
-            "fly_to": "PAR",
-            "dateFrom": "18/03/2023",
-            "dateTo": "18/09/2023",
-            "sort": "price",
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "dateFrom": from_time.strftime("%d/%m/%Y"),
+            "dateTo": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "EUR",
         }
         response = requests.get(url=search_endpoint, params=params, headers=headers)
         response.raise_for_status()
-        lowest_price = response.json()["data"][0]["price"]
-        city = response.json()["data"][0]["cityTo"]
-        return f"{city} : €{lowest_price}"
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}.")
+            return None
 
-flight_searcher = FlightSearch()
-print(flight_searcher.get_prices())
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: €{flight_data.price}")
+        return flight_data
--- Day_39_&_40-Flight_Deal_Finder/flight-deals-start/main.py
+++ Day_39_&_40-Flight_Deal_Finder/flight-deals-start/main.py
@@ -1,16 +1,28 @@
 from data_manager import DataManager
 from flight_search import FlightSearch
+from flight_data import FlightData
+
+from datetime import datetime
+from datetime import timedelta
 from pprint import pprint
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
-
+ORIGIN_CITY_IATA = "DUB"
 data_manager = DataManager()
 flight_searcher = FlightSearch()
 sheet_data = data_manager.get_destination_data()
+
 if sheet_data[0]["iataCode"] == "":
     for data in sheet_data:
         data["iataCode"] = flight_searcher.get_iata_code(data["city"])
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for data in sheet_data:
+    flight = flight_searcher.get_lowest_price(origin_city_code=ORIGIN_CITY_IATA,destination_city_code=data["iataCode"],
+                                              from_time=tomorrow, to_time=six_month_from_today)
 
 # pprint(sheet_data)
 
