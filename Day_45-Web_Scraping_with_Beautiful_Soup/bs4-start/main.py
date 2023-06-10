9ec2f40f7d8e60160aa990c762a051ed2688c427
eoinbreen <e.breen185@gmail.com>
2023-06-10 21:31:46 +0100

    Learned to Parse through a website using Beautiful Soup

---
+++ Day_45-Web_Scraping_with_Beautiful_Soup/bs4-start/main.py
@@ -0,0 +1,10 @@
+from bs4 import BeautifulSoup
+
+with open("website.html", encoding="utf8") as file:
+    contents = file.read()
+
+soup = BeautifulSoup(contents, 'html.parser')
+print(soup.title.string)
+print(soup.p)
+
+
