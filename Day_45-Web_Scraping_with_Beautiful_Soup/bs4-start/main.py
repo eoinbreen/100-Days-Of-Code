ba6e0a75228fe1ec7c0e08f7708dc04282cff39e
eoinbreen <e.breen185@gmail.com>
2023-06-11 22:01:21 +0100

    Learned more about how to use Beautiful Soup

--- Day_45-Web_Scraping_with_Beautiful_Soup/bs4-start/main.py
+++ Day_45-Web_Scraping_with_Beautiful_Soup/bs4-start/main.py
@@ -4,7 +4,19 @@ with open("website.html", encoding="utf8") as file:
     contents = file.read()
 
 soup = BeautifulSoup(contents, 'html.parser')
-print(soup.title.string)
-print(soup.p)
 
+# print(soup.title.string)
+# print(soup.p)
+
+all_anchor_tags = soup.findAll(name="a")
+
+for tag in all_anchor_tags:
+    # print(tag.getText())
+    print(tag.get("href"))
+
+
+headings = soup.findAll(name="h3", class_="heading")
+# headings = soup.select(selector='.heading')
+
+print(headings)
 
