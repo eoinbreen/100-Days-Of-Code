7e3f3bbdb08ac9485a6c87d6c9c897881e9c933b
eoinbreen <e.breen185@gmail.com>
2023-06-15 19:00:27 +0100

    Cleaned up lists and wrote them to txt files

--- Day_45-Web_Scraping_with_Beautiful_Soup/Spotify_Time_Machine/main.py
+++ Day_45-Web_Scraping_with_Beautiful_Soup/Spotify_Time_Machine/main.py
@@ -2,19 +2,25 @@ import requests
 from bs4 import BeautifulSoup
 
 # date = input("What date do you want to travel to? Please type the date in this format YYYY-MM-DD: ")
-date = "1992-05-18"
+date = "2023-06-15"
 URL = "https://www.billboard.com/charts/hot-100/" + date
 
 response = requests.get(URL)
 soup = BeautifulSoup(response.text, "html.parser")
 
-title_list = [song.getText() for song in soup.select(selector='.o-chart-results-list__item>h3')]
-artist_messy_list = [song.getText() for song in soup.findAll(name="span", class_="c-label")]
+title_list = [title.getText().strip() for title in soup.select(selector='.o-chart-results-list__item>h3')]
+artist_messy_list = [artist.getText() for artist in soup.findAll(name="span", class_="c-label") if artist.getText().strip() != "NEW" and artist.getText().strip() != "RE-\nENTRY"]
 
 artist_list = []
 
 for i in range(4, 800, 8):
-    artist_list.append(artist_messy_list[i])
+    artist_list.append(artist_messy_list[i].strip())
 
 print(title_list)
 print(artist_list)
+
+with open(date+" Top 100.txt", mode="w", encoding="utf-8") as file:
+    index = 1
+    for title in title_list:
+        file.write(str(index) + ". " + title + " - " + artist_list[index-1] + "\n")
+        index = index + 1
