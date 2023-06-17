5f69c476401b8d737155cb3566288325153f5a63
eoinbreen <e.breen185@gmail.com>
2023-06-17 22:03:36 +0100

    Learned to search through Spotify for each song in the top 100 and got the uri for each of the songs in a list

--- Day_45-Web_Scraping_with_Beautiful_Soup/Spotify_Time_Machine/main.py
+++ Day_45-Web_Scraping_with_Beautiful_Soup/Spotify_Time_Machine/main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+import pprint
 
 
 # Spotipy Documentation - https://spotipy.readthedocs.io/en/2.22.1/
@@ -22,43 +23,37 @@ sp = spotipy.Spotify(auth_manager=auth_manager)
 
 user_id = sp.current_user()["id"]
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # date = input("What date do you want to travel to? Please type the date in this format YYYY-MM-DD: ")
 
 
-# date = "2023-06-15"
-# URL = "https://www.billboard.com/charts/hot-100/" + date
-#
-# response = requests.get(URL)
-# soup = BeautifulSoup(response.text, "html.parser")
-#
-# title_list = [title.getText().strip() for title in soup.select(selector='.o-chart-results-list__item>h3')]
-# artist_messy_list = [artist.getText() for artist in soup.findAll(name="span", class_="c-label") if artist.getText().strip() != "NEW" and artist.getText().strip() != "RE-\nENTRY"]
-#
-# artist_list = []
-#
-# for i in range(4, 800, 8):
-#     artist_list.append(artist_messy_list[i].strip())
-#
-# print(title_list)
-# print(artist_list)
-#
+date = "2023-06-15"
+URL = "https://www.billboard.com/charts/hot-100/" + date
+
+response = requests.get(URL)
+soup = BeautifulSoup(response.text, "html.parser")
+
+title_list = [title.getText().strip() for title in soup.select(selector='.o-chart-results-list__item>h3')]
+artist_messy_list = [artist.getText() for artist in soup.findAll(name="span", class_="c-label") if artist.getText().strip() != "NEW" and artist.getText().strip() != "RE-\nENTRY"]
+
+artist_list = []
+
+for i in range(4, 800, 8):
+    artist_list.append(artist_messy_list[i].strip())
+
 # with open(date+" Top 100.txt", mode="w", encoding="utf-8") as file:
 #     index = 1
 #     for title in title_list:
 #         file.write(str(index) + ". " + title + " - " + artist_list[index-1] + "\n")
 #         index = index + 1
+year = date.split("-")[0]
+pp = pprint.PrettyPrinter(indent=4)
+song_uris = []
+for song in title_list:
+    result = sp.search(q=f"track:{title_list[0]} year:{year}", type="track")
+    try:
+        uri = result["tracks"]["items"][0]["uri"]
+        song_uris.append(uri)
+    except IndexError:
+        print(f"{song} doesn't exist in Spotify. Skipped")
+
+pp.pprint(song_uris)
