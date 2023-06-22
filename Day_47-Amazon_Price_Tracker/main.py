ba1008eefa6d5c13b01905257158b0692f24f041
eoinbreen <e.breen185@gmail.com>
2023-06-22 22:22:33 +0100

    Learned how to find data using Selenium

--- Day_47-Amazon_Price_Tracker/main.py
+++ Day_47-Amazon_Price_Tracker/main.py
@@ -5,7 +5,7 @@ import smtplib
 MY_EMAIL = "eoinbreen185@yahoo.com"
 email_password = "dwfpbsemhisdcvbo"
 
-URL="https://www.amazon.co.uk/Samsung-Galaxy-Android-Smartphone-Phantom/dp/B09NRRVPZ7/ref=sr_1_3?crid=2JF3OKZJBLW8C&keywords=samsung+galaxy+s22&qid=1687206141&sprefix=samsung+galaxy+s22%2Caps%2C77&sr=8-3"
+URL = "https://www.amazon.co.uk/Samsung-Galaxy-Android-Smartphone-Phantom/dp/B09NRRVPZ7/ref=sr_1_3?crid=2JF3OKZJBLW8C&keywords=samsung+galaxy+s22&qid=1687206141&sprefix=samsung+galaxy+s22%2Caps%2C77&sr=8-3"
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
--- Day_48-Selenium_Webdriver_&_Game_Playing_Bot/Day_48_Demo/main.py
+++ Day_48-Selenium_Webdriver_&_Game_Playing_Bot/Day_48_Demo/main.py
@@ -1,10 +1,24 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 chrome_driver_path = "C:\Development\chromedriver.exe"
 
 driver = webdriver.Chrome()
 
-driver.get("https://www.amazon.com")
+# driver.get("https://www.amazon.co.uk/Samsung-Galaxy-Android-Smartphone-Phantom/dp/B09NRRVPZ7/ref=sr_1_3?crid=2JF3OKZJBLW8C&keywords=samsung+galaxy+s22&qid=1687206141&sprefix=samsung+galaxy+s22%2Caps%2C77&sr=8-3")
+# price = driver.find_element(By.CLASS_NAME, "a-price-whole")
+# print("Price: " + price.text)
 
 
+# driver.get("https://www.python.org/")
+# searchbar = driver.find_element(By.NAME, "q")
+# print(searchbar.get_attribute("placeholder"))
 
+# driver.get("https://www.python.org/")
+# link = driver.find_element(By.CSS_SELECTOR, ".documentation-widget a")
+# print(link.text)
+
+
+driver.get("https://www.python.org/")
+xpath = driver.find_element(By.XPATH, "//*[@id='site-map']/div[2]/div/ul/li[3]/a")
+print(xpath.text)
